7dfeaab57fb4fdecdff9af5b2889ed680bd348b5
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-02-09 20:42:12 -0600

    Made add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,9 +48,34 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">Add to Cart</button>
+    <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${
+      product.id
+    }">Add to Cart</button>
   </div>
 `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart-button').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const productId = btn.dataset.productId;
+    let machingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        machingItem = item;
+      }
+    });
+
+    if (machingItem) {
+      machingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+
+    console.log(cart);
+  });
+});
