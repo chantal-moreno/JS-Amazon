c76f0412b0802df2a58e880542007d3fda73636b
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-02-09 20:50:20 -0600

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,11 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((btn) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity);
   });
 });
