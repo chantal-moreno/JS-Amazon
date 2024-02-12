23af2c68d517aaec240ddbcadd00e4a6fb644c8c
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2024-02-12 12:19:32 -0600

    Added Message: Show message and setTimeout

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -43,7 +43,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png" />
       Added
     </div>
@@ -57,6 +57,7 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};
 document.querySelectorAll('.js-add-to-cart-button').forEach((btn) => {
   btn.addEventListener('click', () => {
     const { productId } = btn.dataset;
@@ -85,6 +86,20 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((btn) => {
       cartQuantity += item.quantity;
     });
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add('added-to-cart-visible');
+    const previousTimeoutId = addedMessageTimeouts[productId];
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+    addedMessageTimeouts[productId] = timeoutId;
+
     console.log(cart);
   });
 });
